70672c3864327f0651ac7d582614ec533751fe85
Edu Starcevich <edu@mail.com>
2022-08-01 17:52:55 -0300

    entorno de trabajo configurado

--- blog8/blog8/settings/settings.py
+++ blog8/blog8/settings/settings.py
@@ -130,3 +130,5 @@ MEDIAROOT = os.path.join (os.path.dirname (BASE_DIR),"media")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
