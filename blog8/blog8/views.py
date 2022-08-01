6d69e1569e78a154e4a2eec7faf36b66de103e14
Edu Starcevich <edu@mail.com>
2022-08-01 18:38:23 -0300

    archivos static + templatePadre agregados

--- blog8/blog8/views.py
+++ blog8/blog8/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def vista (request):
-    return render (request,"plantillaPadre.html",context={})
+    return render (request,"templatePadre.html",context={})
